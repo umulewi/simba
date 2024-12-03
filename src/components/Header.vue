04dca21433cc9ddeef744cea92255d6bb351cb0d
LEWIS NTEGEREJIMANA <umulewi@gmail.com>
2024-12-03 11:33:16 +0200

    create vue components

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello-world">
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HelloWorld",
+    data() {
+      return {
+        message: "Hello, Vue!",
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .hello-world {
+    text-align: center;
+    font-family: Arial, sans-serif;
+  }
+  </style>
+  
